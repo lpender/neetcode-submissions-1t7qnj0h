66b8919bc86b3014c85b29000bf4d33b03b6c68b
Lee Pender <lpender@gmail.com>
2026-04-02 13:32:09 -0400

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const set = new Set();
+
+        for (const i of nums) {
+            if (set.has(i)) {
+                return true
+            }
+            set.add(i)
+        }
+        return false
+    }
+}
